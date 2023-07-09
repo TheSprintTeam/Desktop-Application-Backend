03dfc4db3b7528b1121b6a00b292876a3d70f62e
Peter Li <peterli135@hotmail.com>
2023-07-08 17:22:12 -0700

    fix team serializers

--- app/serializers/teamSerializers.py
+++ app/serializers/teamSerializers.py
@@ -16,10 +16,33 @@ def teamResponseEntity(team) -> dict:
         "name": team["name"],
         "description": team["description"],
         "team_lead": str(team["team_lead"]),
-        "invites": [str(i) for i in team["invites"]],
-        "members": [str(i) for i in team["members"]],
+        "invites": inviteListResponseEntity(team["invites"]),
+        "members": memberListResponseEntity(team["members"]),
         "technologies": [str(i) for i in team["technologies"]]
     }
 
 def teamListResponseEntity(teams) -> list:
-    return [teamEntity(team) for team in teams]
+    return [teamResponseEntity(team) for team in teams]
+
+
+# entities for the invites, members, and technologies (they are lists which may have objectIds)
+def inviteEntity(invite) -> dict:
+    print(invite)
+    return {
+        "email": invite["email"],
+        "role": str(invite["role"]),
+        "otp_code": invite["otp_code"],
+    }
+
+def inviteListResponseEntity(invites) -> list:
+    return [inviteEntity(invite) for invite in invites]
+
+def memberEntity(member) -> dict:
+    return {
+        "user_id": str(member["user_id"]),
+        "role": str(member["role"]),
+        "email": member["email"]
+    }
+
+def memberListResponseEntity(members) -> list:
+    return [memberEntity(member) for member in members]
