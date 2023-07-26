9849277d1e94483e4a5697bbc0d9e43918b43157
Peter Li <peterli135@hotmail.com>
2023-07-25 17:00:59 -0700

    add cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import google_auth, users, teams, machine_learning
 
 # uvicorn app.main:app --reload
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(google_auth.router)
