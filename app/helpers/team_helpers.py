40e82c528309d6e3293b66a8a0d51dd2dede4c55
Peter Li <p279li@uwaterloo.ca>
2023-09-16 14:26:12 -0400

    fix getting all user teams

--- app/helpers/team_helpers.py
+++ app/helpers/team_helpers.py
@@ -6,7 +6,7 @@ def get_all_user_teams(user_id):
     result_teams = find_data("teams",
         {"$or": [
             {"team_lead": user_id},
-            {"members": user_id}
+            {"members": { "$elemMatch": {"user_id": user_id } } }
         ]})
     if result_teams is not Exception:
         return teamListResponseEntity(result_teams)
